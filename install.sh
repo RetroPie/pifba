3e137fa7a2ad5e96909bc727f025a71972061b25
Squid <squidrpi@gmail.com>
2013-07-15 14:35:43 +0100

    Added

---
+++ install.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+cd /usr/local/bin/indiecity/InstalledApps/pifba/Full
+chmod 777 ./fba2x ./fbacapex ./capex.cfg ./fba2x.cfg ./zipname.fba ./rominfo.fba ./FBACache_windows.zip ./roms ./skin ./preview ./preview/*
